9fe21a5ef303933cc3e4717ec30eef39aa497b22
ko-to-mi-san <47980784+ko-to-mi-san@users.noreply.github.com>
2022-01-31 13:22:07 +0900

    リストにタスク追加機能

--- src/index.js
+++ src/index.js
@@ -1,4 +1,40 @@
 import sheet from "./styles.css" assert { type: 'css' };
 
 document.adoptedStyleSheets = [sheet];
-shadowRoot.adoptedStyleSheets = [sheet];
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value
+  document.getElementById("add-text").value = "";
+
+  const li = document.createElement("li");
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const p = document.createElement("p");
+  p.className = "todo-string";
+  p.innerText = inputText;
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了")
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除")
+  });
+
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  li.appendChild(div);
+
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
