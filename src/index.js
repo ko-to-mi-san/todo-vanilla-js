70806a57a55b10c2511fb0dd7040ad9f527622c5
ko-to-mi-san <47980784+ko-to-mi-san@users.noreply.github.com>
2022-02-01 09:25:02 +0900

    削除機能

--- src/index.js
+++ src/index.js
@@ -24,7 +24,8 @@ const onClickAdd = () => {
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    alert("削除")
+    const deleteTarget = div.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
   });
 
   div.appendChild(p);
